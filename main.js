c0ef2bfc9eb5bfffe99118c0f038b99f4a937391
townofclay <crgilbert@protonmail.com>
2022-11-02 13:14:18 -0400

    Add metconIdeas object

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
 }
+
+const metconIdeas = {
+    timeDomain: ['5 minutes', '10 minutes', '15 minutes', '20 minutes', '30+ minutes'] ,
+    metconType: ['an EMOM', 'an AMRAP', '3 rounds', '5 rounds', '10 rounds'],
+    repScheme: ['10-9-8-7-6-5-4-3-2-1', '21-15-9', '3-6-9', '15', '100'] ,
+    movementType: ['thrusters', 'clean & jerks', 'snatches', 'box jumps', 'burpees'],
+    movementType2: ['deadlifts', 'HSPU', 'pull-ups', 'toes-to-bar', 'GHD sit-ups']
+}
+
