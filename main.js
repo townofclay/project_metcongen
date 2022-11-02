5d338eaedeea1e8614df8aaa896a93e3ed86b831
townofclay <crgilbert@protonmail.com>
2022-11-02 13:34:59 -0400

    Add iteration over object for randomizing

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// Get random number (to help randomize output)
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * num)
 }
 
+// Object for all potential metcon ideas
 const metconIdeas = {
     timeDomain: ['5 minutes', '10 minutes', '15 minutes', '20 minutes', '30+ minutes'] ,
     metconType: ['an EMOM', 'an AMRAP', '3 rounds', '5 rounds', '10 rounds'],
@@ -10,3 +12,31 @@ const metconIdeas = {
     movementType2: ['deadlifts', 'HSPU', 'pull-ups', 'toes-to-bar', 'GHD sit-ups']
 }
 
+// Store the metcon in an array
+let wod = []
+
+// Iterate over the object
+for(let property in metconIdeas) {
+    let optionIndex = generateRandomNumber(metconIdeas[property].length)
+
+    // use object's properties to customize workout being added to WOD
+    switch(property) {
+        case 'timeDomain':
+            wod.push(`You have ${metconIdeas[property][optionIndex]}`)
+            break
+        case 'metconType':
+            wod.push(`to complete ${metconIdeas[property][optionIndex]}`)
+            break
+        case 'repScheme':
+            wod.push(`of ${metconIdeas[property][optionIndex]}`)
+            break
+        case 'movementType':
+            wod.push(`${metconIdeas[property][optionIndex]}`)
+            break
+        case 'movementType2':
+            wod.push(`and ${metconIdeas[property][optionIndex]}.`)
+            break
+        default:
+            wod.push('Error: it\'s a rest day.')
+    }
+}
